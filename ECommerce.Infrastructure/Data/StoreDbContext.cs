a117e243e26caf1a4f05b917f4089110ee60fd31
Mahmoud Abdel Sami <Mahmoud.Abdel.Sami113@science.helwan.edu.eg>
2026-06-28 06:09:03 +0300

    Product Module

---
+++ ECommerce.Infrastructure/Data/StoreDbContext.cs
@@ -0,0 +1,23 @@
+﻿using ECommerce.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Infrastructure.Data
+{
+    public class StoreDbContext(DbContextOptions<StoreDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(StoreDbContext).Assembly);
+        }
+    }
+}
