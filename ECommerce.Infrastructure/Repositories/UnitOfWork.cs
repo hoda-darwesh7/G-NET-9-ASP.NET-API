a8c0bcb9015d5eb911c407ba51138a73de6c2e97
Mahmoud Abdel Sami <Mahmoud.Abdel.Sami113@science.helwan.edu.eg>
2026-07-03 19:01:06 +0300

    GenericRepository & UnitOfWork

---
+++ ECommerce.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using ECommerce.Domain.Contracts;
+using ECommerce.Domain.Entities;
+using ECommerce.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Infrastructure.Repositories
+{
+    public class UnitOfWork(StoreDbContext dbContext) : IUnitOfWork
+    {
+        private readonly Dictionary<string , object> repositories = [];
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        {
+            var typeName = typeof(TEntity).Name;
+            if ( repositories.TryGetValue(typeName, out object? value))
+                return (IGenericRepository<TEntity , TKey>) value;
+            else
+            {
+                var repo = new GenericRepository<TEntity , TKey>(dbContext);
+                repositories[typeName] = repo;
+                return repo;
+            }
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken ct = default) => await dbContext.SaveChangesAsync(ct);
+    }
+}
