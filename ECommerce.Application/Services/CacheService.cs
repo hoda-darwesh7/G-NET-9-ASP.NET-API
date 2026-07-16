38c149d487b76664d8c786613a36818942d85c81
Mahmoud Abdel Sami <Mahmoud.Abdel.Sami113@science.helwan.edu.eg>
2026-07-16 22:17:45 +0300

    CacheService

---
+++ ECommerce.Application/Services/CacheService.cs
@@ -0,0 +1,31 @@
+﻿using ECommerce.Application.Contracts;
+using ECommerce.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.Services
+{
+    public class CacheService : ICacheService
+    {
+        private readonly ICacheRepository _cacheRepo;
+
+        public CacheService(ICacheRepository cacheRepo)
+        {
+            _cacheRepo = cacheRepo;
+        }
+
+        public async Task<string?> GetDataAsync(string cachekey, CancellationToken ct = default) 
+            => await _cacheRepo.GetAsync(cachekey, ct);
+
+
+        public async Task SetDataAsync(string cachekey, string cacheValue, TimeSpan? TimeToLive = null, CancellationToken ct = default)
+        {
+            var jsonValue = JsonSerializer.Serialize(cacheValue);
+            await _cacheRepo.SetAsync(cachekey, jsonValue, TimeToLive, ct);
+        }
+    }
+}
--- ECommerce.Infrastructure/InfrastructureServiceRegister.cs
+++ ECommerce.Infrastructure/InfrastructureServiceRegister.cs
@@ -31,6 +31,7 @@ namespace ECommerce.Infrastructure
             });
 
             services.AddScoped<IBasketRepository, BasketRepository>();
+            services.AddScoped<ICacheRepository, CacheRepository>();
 
             return services;
         }
